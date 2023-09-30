0d725a85851c41bb4eebf2cc41ffcc56182f6312
Любовь Соколова <sokolova.l.r@yandex.ru>
2023-09-30 21:22:47 +0200

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE =  "https://cf9f0ab8-48f1-4435-a036-3f0330e824d3.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
